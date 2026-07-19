38555a5e859bfa6c8a730f2256332b1c7154dbe3
Govind Tanda <govindstanda@outlook.com>
2026-07-19 17:38:48 -0600

    Add Jump/sprtin
    add ability to jump and sprint

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,14 +4,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")] 
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float sprintSpeed = 7f;
+    private float currentSpeed;
+    private bool isSprinting;
+
+    [Header("Jump")] [SerializeField] private float jumpHeight = 1.2f;
+    
     private CharacterController _characterController;
     private PlayerControls _playerControls;
 
+
+
     private Vector2 moveInput;
     private Vector2 lookInput;
-    
+
     public Vector2 LookInput => lookInput;
-    
+
     public float moveSpeed = 5f;
     public float gravity = -9.81f;
 
@@ -27,6 +37,11 @@ public class PlayerController : MonoBehaviour
 
         _playerControls.Player.Rotation.performed += OnLook;
         _playerControls.Player.Rotation.canceled += OnLookCancelled;
+        
+        _playerControls.Player.Sprint.performed += OnSprint;
+        _playerControls.Player.Sprint.canceled += OnSprintCancelled;
+        
+        _playerControls.Player.Jump.performed += OnJump;
 
     }
 
@@ -34,7 +49,7 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
     }
-    
+
     private void OnMoveCancelled(InputAction.CallbackContext context)
     {
         moveInput = Vector2.zero;
@@ -44,13 +59,33 @@ public class PlayerController : MonoBehaviour
     {
         lookInput = context.ReadValue<Vector2>();
     }
-    
+
     private void OnLookCancelled(InputAction.CallbackContext context)
     {
         lookInput = Vector2.zero;
     }
 
-    private void OnEnable()
+    private void OnSprint(InputAction.CallbackContext context)
+    {
+        isSprinting = true;
+        currentSpeed = sprintSpeed;
+    }
+    
+    private void OnSprintCancelled(InputAction.CallbackContext context)
+    {
+        isSprinting = false;
+        currentSpeed = moveSpeed;
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (_characterController.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+private void OnEnable()
     {
         _playerControls.Enable();
     }
@@ -63,11 +98,11 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
-        _characterController.Move(move * moveSpeed * Time.deltaTime);
+        _characterController.Move(move * currentSpeed * Time.deltaTime);
 
         if (_characterController.isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = -1f;
         }
         
         velocity.y += gravity * Time.deltaTime;
