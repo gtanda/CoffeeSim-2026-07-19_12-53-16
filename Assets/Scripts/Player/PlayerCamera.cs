3bbe9562202ea46c50fd1f1fa29fc031c2d1549a
Govind Tanda <govindstanda@outlook.com>
2026-07-19 17:32:42 -0600

    Add controller/first person view
    Used unity input system and character controller

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("References")]
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] Transform playerBody;
+    
+    [Header("Settings")]
+    [SerializeField] private float mouseSensitivity = 0.15f;
+    private float xRotation = 0f;
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
     // Update is called once per frame
     void Update()
     {
+        HandleLook();
+    }
+
+    private void HandleLook()
+    {
+        Vector2 lookInput = playerController.LookInput;
         
+        float mouseX = lookInput.x * mouseSensitivity;
+        float mouseY = lookInput.y * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,76 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private CharacterController _characterController;
+    private PlayerControls _playerControls;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    
+    public Vector2 LookInput => lookInput;
+    
+    public float moveSpeed = 5f;
+    public float gravity = -9.81f;
+
+    private Vector3 velocity;
+
+    private void Awake()
     {
-        
+        _characterController = GetComponent<CharacterController>();
+        _playerControls = new PlayerControls();
+
+        _playerControls.Player.Movement.performed += OnMove;
+        _playerControls.Player.Movement.canceled += OnMoveCancelled;
+
+        _playerControls.Player.Rotation.performed += OnLook;
+        _playerControls.Player.Rotation.canceled += OnLookCancelled;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMove(InputAction.CallbackContext context)
     {
+        moveInput = context.ReadValue<Vector2>();
+    }
+    
+    private void OnMoveCancelled(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero;
+    }
+
+    private void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+    
+    private void OnLookCancelled(InputAction.CallbackContext context)
+    {
+        lookInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        _characterController.Move(move * moveSpeed * Time.deltaTime);
+
+        if (_characterController.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         
+        velocity.y += gravity * Time.deltaTime;
+        _characterController.Move(velocity * Time.deltaTime);
     }
 }
