4c75de69627c3b3ef86e65df9d2ed2786c3e794e
Govind Tanda <govindstanda@outlook.com>
2026-07-19 17:42:07 -0600

    refactor
    refactor playercontroller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,111 +1,150 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement")] 
-    [SerializeField] private float walkSpeed = 4f;
-    [SerializeField] private float sprintSpeed = 7f;
-    private float currentSpeed;
-    private bool isSprinting;
-
-    [Header("Jump")] [SerializeField] private float jumpHeight = 1.2f;
-    
     private CharacterController _characterController;
     private PlayerControls _playerControls;
 
+
+    [Header("Movement")]
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float sprintSpeed = 7f;
+
+
+    [Header("Jump")]
+    [SerializeField] private float jumpHeight = 1.2f;
+    [SerializeField] private float gravity = -9.81f;
 
 
     private Vector2 moveInput;
     private Vector2 lookInput;
 
-    public Vector2 LookInput => lookInput;
+    private float currentSpeed;
 
-    public float moveSpeed = 5f;
-    public float gravity = -9.81f;
+    private bool isSprinting;
 
     private Vector3 velocity;
+
+
+    public Vector2 LookInput => lookInput;
+
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
+
         _playerControls = new PlayerControls();
 
-        _playerControls.Player.Movement.performed += OnMove;
-        _playerControls.Player.Movement.canceled += OnMoveCancelled;
+        currentSpeed = walkSpeed;
 
-        _playerControls.Player.Rotation.performed += OnLook;
-        _playerControls.Player.Rotation.canceled += OnLookCancelled;
-        
-        _playerControls.Player.Sprint.performed += OnSprint;
-        _playerControls.Player.Sprint.canceled += OnSprintCancelled;
-        
-        _playerControls.Player.Jump.performed += OnJump;
-
+        SetupInput();
     }
 
-    private void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
 
-    private void OnMoveCancelled(InputAction.CallbackContext context)
+    private void SetupInput()
     {
-        moveInput = Vector2.zero;
-    }
-
-    private void OnLook(InputAction.CallbackContext context)
-    {
-        lookInput = context.ReadValue<Vector2>();
-    }
-
-    private void OnLookCancelled(InputAction.CallbackContext context)
-    {
-        lookInput = Vector2.zero;
-    }
-
-    private void OnSprint(InputAction.CallbackContext context)
-    {
-        isSprinting = true;
-        currentSpeed = sprintSpeed;
-    }
-    
-    private void OnSprintCancelled(InputAction.CallbackContext context)
-    {
-        isSprinting = false;
-        currentSpeed = moveSpeed;
-    }
-
-    private void OnJump(InputAction.CallbackContext context)
-    {
-        if (_characterController.isGrounded)
+        // Movement
+        _playerControls.Player.Movement.performed += ctx =>
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+            moveInput = ctx.ReadValue<Vector2>();
+        };
+
+        _playerControls.Player.Movement.canceled += ctx =>
+        {
+            moveInput = Vector2.zero;
+        };
+
+
+        // Look
+        _playerControls.Player.Rotation.performed += ctx =>
+        {
+            lookInput = ctx.ReadValue<Vector2>();
+        };
+
+        _playerControls.Player.Rotation.canceled += ctx =>
+        {
+            lookInput = Vector2.zero;
+        };
+
+
+        // Sprint
+        _playerControls.Player.Sprint.performed += ctx =>
+        {
+            isSprinting = true;
+            currentSpeed = sprintSpeed;
+        };
+
+        _playerControls.Player.Sprint.canceled += ctx =>
+        {
+            isSprinting = false;
+            currentSpeed = walkSpeed;
+        };
+
+
+        // Jump
+        _playerControls.Player.Jump.performed += ctx =>
+        {
+            Jump();
+        };
     }
 
-private void OnEnable()
+
+    private void OnEnable()
     {
         _playerControls.Enable();
     }
+
 
     private void OnDisable()
     {
         _playerControls.Disable();
     }
 
+
     private void Update()
     {
-        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
-        _characterController.Move(move * currentSpeed * Time.deltaTime);
+        HandleMovement();
+        HandleGravity();
+    }
 
+
+    private void HandleMovement()
+    {
+        Vector3 move =
+            transform.right * moveInput.x +
+            transform.forward * moveInput.y;
+
+
+        _characterController.Move(
+            move * currentSpeed * Time.deltaTime
+        );
+    }
+
+
+    private void HandleGravity()
+    {
         if (_characterController.isGrounded && velocity.y < 0)
         {
             velocity.y = -1f;
         }
-        
+
+
         velocity.y += gravity * Time.deltaTime;
-        _characterController.Move(velocity * Time.deltaTime);
+
+
+        _characterController.Move(
+            velocity * Time.deltaTime
+        );
+    }
+
+
+    private void Jump()
+    {
+        if (_characterController.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(
+                jumpHeight * -2f * gravity
+            );
+        }
     }
 }
