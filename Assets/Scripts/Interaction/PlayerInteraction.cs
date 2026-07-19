6b718fce34bbb46423397a78de23a8e52d346b8b
Govind Tanda <govindstanda@outlook.com>
2026-07-19 17:58:26 -0600

    interaction
    add basic interaction

---
+++ Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interaction
+{
+    public class PlayerInteraction : MonoBehaviour
+    {
+        public float interactDistance = 3f;
+        private PlayerControls _playerControls;
+        [SerializeField] private Camera playerCamera;
+
+        private void Awake()
+        {
+            _playerControls = new PlayerControls();
+            
+            _playerControls.Player.Interact.performed += ctx => TryInteract();
+        }
+
+        private void OnEnable()
+        {
+            _playerControls.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerControls.Disable();
+        }
+
+        private void TryInteract()
+        {
+            Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            
+            if (Physics.Raycast(ray, out RaycastHit hit, interactDistance))
+            {
+                IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+                if (interactable != null)
+                {
+                    interactable.Interact();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/TestInteractable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Interaction
+{
+    public class TestInteractable : MonoBehaviour, IInteractable
+    {
+        public void Interact()
+        {
+            Debug.Log("I interacted with: " + gameObject.name);
+        }
+    }
+}
