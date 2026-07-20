517974c315de0ac88657b0ae548bdd6ea45089fb
Govind Tanda <govindstanda@outlook.com>
2026-07-19 18:41:31 -0600

    Setup basic interaction

--- Assets/Scripts/Interaction/PlayerInteraction.cs
+++ Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace Interaction
@@ -9,11 +10,19 @@ namespace Interaction
         private PlayerControls _playerControls;
         [SerializeField] private Camera playerCamera;
 
+        [SerializeField] private GameObject interactionPrompt;
+        [SerializeField] private TextMeshProUGUI promptText;
+
         private void Awake()
         {
             _playerControls = new PlayerControls();
-            
+
             _playerControls.Player.Interact.performed += ctx => TryInteract();
+        }
+
+        private void Update()
+        {
+            CheckForInteractable();
         }
 
         private void OnEnable()
@@ -26,17 +35,57 @@ namespace Interaction
             _playerControls.Disable();
         }
 
-        private void TryInteract()
+        private void OnDrawGizmos()
         {
+            if (playerCamera == null)
+                return;
+
+            Gizmos.color = Color.yellow;
+
+            Vector3 start = playerCamera.transform.position;
+            Vector3 end = start + playerCamera.transform.forward * interactDistance;
+
+            Gizmos.DrawLine(start, end);
+
+            Gizmos.DrawWireSphere(end, 0.3f);
+        }
+
+        private bool TryGetInteractable(out IInteractable interactable)
+        {
+            interactable = null;
+
             Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
-            
-            if (Physics.Raycast(ray, out RaycastHit hit, interactDistance))
+
+            if (Physics.SphereCast(ray, 0.3f, out RaycastHit hit, interactDistance))
             {
-                IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+                interactable = hit.collider.GetComponent<IInteractable>();
+                return interactable != null;
+            }
+
+            return false;
+        }
+
+        private void CheckForInteractable()
+        {
+            if (TryGetInteractable(out IInteractable interactable))
+            {
                 if (interactable != null)
                 {
-                    interactable.Interact();
+                    interactionPrompt.SetActive(true);
+                    promptText.text = "Press E to interact";
                 }
+                else
+                {
+                    interactionPrompt.SetActive(false);
+                }
+            }
+        }
+
+        private void TryInteract()
+        {
+            if (TryGetInteractable(out IInteractable interactable))
+            {
+                interactable.Interact();
             }
         }
     }
